1a68b1cb8d13e0557f67e7334bfaf38b01a5f211
Rohan Borse <111951030+Rohanborse@users.noreply.github.com>
2025-02-20 16:13:26 +0530

    Update Gen_AL_1.py

--- Gen_AL_1.py
+++ Gen_AL_1.py
@@ -82,4 +82,4 @@ def download_file():
     return send_file(excel_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
